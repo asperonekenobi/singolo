7e0e53469fbbd5520405ba215ccebe63bc5095b2
AndreySinkovets <andreysinkovets@yahoo.com>
2020-03-21 23:37:46 +0300

    feat: add target and switchScreen

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const onScroll = () => {
-  const curPos = window.scrollY + 95;
+const links = document.querySelector('.nav-list__row');
+
+// Change color menu scroll
+document.addEventListener('scroll', onScroll);
+function onScroll() {
+  const curPos = window.scrollY + 96;
   const sections = document.querySelectorAll('main>section');
   const links = document.querySelectorAll('.nav-list__row a');
 
@@ -18,7 +22,38 @@ const onScroll = () => {
       document.querySelector('#nav-contact').classList.add('nav-list__link--active');
     }
   });
+}
 
-};
+//Change color menu target
+links.addEventListener('click', onClick);
+function onClick(event) {
+  document.querySelectorAll('.nav-list__row a').forEach(a => a.classList.remove('nav-list__link--active'));
+  if (event.target !== event.currentTarget) {
+    event.target.classList.add('nav-list__link--active');
+  }
+}
 
-document.addEventListener('scroll', onScroll);
+//Turn OFF display
+  const phoneVertical = document.querySelector('#vertical-phone');
+  const phoneHorizontal = document.querySelector('#horizontal-phone');
+  const screenVertical = document.querySelector('.phone__screen');
+  const screenHorizontal = document.querySelector('.phone--blue .phone__screen');
+
+  phoneVertical.addEventListener('click', () => {
+    if (screenVertical.classList[1] !== 'hide-screen') {
+      screenVertical.classList.add('hide-screen');
+    } else screenVertical.classList.remove('hide-screen');
+  });
+
+  phoneHorizontal.addEventListener('click', () => {
+    if (screenHorizontal.classList[1] !== 'hide-screen') {
+      screenHorizontal.classList.add('hide-screen');
+    } else screenHorizontal.classList.remove('hide-screen');
+  });
+
+//
+
+
+
+
+
